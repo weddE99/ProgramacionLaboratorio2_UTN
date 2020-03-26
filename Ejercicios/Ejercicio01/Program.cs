7206756c1e3ff9e07433f9f98e98b8304932e9d7
weddE99 <54249487+weddE99@users.noreply.github.com>
2020-03-26 17:52:17 -0300

    Add files via upload

---
+++ Ejercicios/Ejercicio01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio nro 01";
+            int suma=0, min=0, max=0, numero, i=0;
+            float prom;
+            String aux;
+
+            Console.WriteLine("Ingrese 5 numeros: ");
+
+            do
+            {
+                aux = Console.ReadLine();
+                if(int.TryParse(aux, out numero))
+                {
+                    if (numero > max || i==0) max = numero;
+
+                    if (numero < min || i==0) min = numero;
+                    suma += numero;
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR!: ingrese un numero");
+                }
+            } while (i < 5);
+            prom = suma / 5;
+
+            Console.WriteLine("\nEl promedio es: {0:#,####.00}", prom);
+            Console.WriteLine("El maximo es: {0}", max);
+            Console.WriteLine("El minimo es: {0}", min);
+        }
+    }
+}
---
+++ Ejercicios/Ejercicio2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String aux;
+            int num;
+            Console.Title = "Ejercicio nro 02";
+            Console.WriteLine("Ingrese un numero: ");
+
+            do
+            {
+                aux = Console.ReadLine();
+                if (int.TryParse(aux, out num))
+                {
+                    if(num > 0)
+                    {
+                        Console.WriteLine("^2 {0}", Math.Pow(num, 2));
+                        Console.WriteLine("^3 {0}", Math.Pow(num, 3));
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR. ¡Reingresar Numero!\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERROR. ¡Reingresar Numero!\n");
+                }
+            } while (true);
+        }
+    }
+}
